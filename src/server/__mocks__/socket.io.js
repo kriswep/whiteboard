e04253531bbe93efdaa913bb8c39c4af02acd6a2
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-07-29 14:01:38 +0200

    improve server socket tests

--- src/server/__mocks__/socket.io.js
+++ src/server/__mocks__/socket.io.js
@@ -1,10 +1,18 @@
 /* globals jest */
-export const mockTo = jest.fn();
+export const mockEmit = jest.fn();
+export const mockTo = jest.fn(() => ({
+  emit: mockEmit,
+}));
 export const mockOn = jest.fn();
 
 const socketio = jest.fn(() => ({
   to: mockTo,
   on: mockOn,
+  sockets: {
+    adapter: {
+      rooms: [],
+    },
+  },
 }));
 
 export default socketio;
--- src/server/socket.spec.js
+++ src/server/socket.spec.js
@@ -1,8 +1,8 @@
 /* globals test expect jest */
-// import { mockTo, mockOn } from 'socket.io';
+import { mockTo, mockOn } from 'socket.io';
 import startIo, { linesToRoom } from './socket';
 
-test('socket schould not smoke', () => {
+test('socket should not smoke', () => {
   expect(startIo).not.toThrow();
   // expect(mockOn).toHaveBeenCalledWith();
 });
@@ -30,4 +30,28 @@ test('Should map lines to rooms', () => {
   expect(mockEmit).toHaveBeenCalled();
   expect(mockEmit.mock.calls[0]).toEqual(['draw_line', { line: 'line' }]);
   expect(mockEmit.mock.calls[1]).toEqual(['draw_line', { line: 'line' }]);
+});
+
+test('startIo should handle socket events', () => {
+  const socket = {
+    on: jest.fn(),
+    leave: jest.fn(),
+    join: jest.fn(),
+    rooms: [1, 2],
+  };
+  mockOn.mockClear();
+  expect(startIo).not.toThrow();
+  expect(mockOn).toHaveBeenCalled();
+  expect(typeof mockOn.mock.calls[0][1]).toBe('function');
+  expect(mockOn.mock.calls[0][1].bind(null, socket)).not.toThrow();
+
+  expect(socket.on).toHaveBeenCalled();
+  /* test draw line, todo test output */
+  expect(socket.on.mock.calls[0][0]).toBe('draw_line');
+  expect(typeof socket.on.mock.calls[0][1]).toBe('function');
+  expect(socket.on.mock.calls[0][1].bind(null, socket)).not.toThrow();
+  /* test switch room, todo test output */
+  expect(socket.on.mock.calls[1][0]).toBe('switch_board');
+  expect(typeof socket.on.mock.calls[1][1]).toBe('function');
+  expect(socket.on.mock.calls[1][1].bind(null, socket)).not.toThrow();
 });
